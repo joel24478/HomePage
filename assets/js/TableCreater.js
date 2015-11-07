4f78fbd1027541a458793a0eff96d53dee497a68
Joel24478 <joel24478@gmail.com>
2015-11-06 21:16:11 -0500

    Added corrections to assignment_7

--- assets/js/TableCreater.js
+++ assets/js/TableCreater.js
@@ -11,10 +11,47 @@ $(document).ready(function() {
         success: "valid"
     });
     $("#tableForm").validate({
+        rules: {
+            rowsFrom: {
+                required: true,
+                range: [0, 20]
+            },
+            rowsTo: {
+                required: true,
+                min: function() {
+                    return $("#rowsFrom").val();
+                },
+                max: 25
+            },
+            columnsFrom: {
+                required: true,
+                range: [0, 40]
+            },
+            columnsTo: {
+                required: true,
+                min: function() {
+                    return $("#columnsFrom").val();
+                },
+                max: 60
+            },
+        },
+        messages: {
+            rowsTo: {
+                min: function() {
+                    return "Please enter a number greater than or equal to " + $("#rowsFrom").val();
+                }
+            },
+            columnsTo: {
+                min: function() {
+                    return "Please enter a number greater than or equal to " + $("#columnsFrom").val();
+                }
+            }
+        },
         //Callback for handling the actual submit when the form is valid
         submitHandler: function(event) {
             TableCreater();
         }
+
     });
 });
 
